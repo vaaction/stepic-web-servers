d33ad31b519cc3624c5f4d36ef298e9e92bf667e
ibukanov <IBukanov@energocomm.ru>
2017-01-05 19:48:49 +0300

    q manager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class QuestionManager(models.Manager):
+
+    def new():
+        return Question.objects.order_by(Question.added_at)[:10]
+
+    def popular():
+        return Question.objects.order_by(Question.rating)
+
+
 class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=255)
@@ -17,12 +26,3 @@ class Answer(models.Model):
     added_at = models.DateTimeField(blank=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
-
-
-class QuestionManager(models.Manager):
-
-    def new():
-        return Question.objects.order_by(Question.added_at)[:10]
-
-    def popular():
-        return Question.objects.order_by(Question.rating)
