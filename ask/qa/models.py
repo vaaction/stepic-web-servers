3985904ff3810531de5d1c6b24c2143954cd9fa5
ibukanov <IBukanov@energocomm.ru>
2017-01-05 19:11:37 +0300

    add db model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, related_name='user_author')
+    likes = models.ManyToManyField(User)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+
+
+class QuestionManager:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def new():
+        return Question.objects.order_by(Question.added_at)[:10]
+
+    @staticmethod
+    def popular():
+        return Question.objects.order_by(Question.rating)
