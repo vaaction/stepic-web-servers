90a1def141dccf96ee0775491356d89605e5b241
ibukanov <IBukanov@energocomm.ru>
2017-01-05 19:21:49 +0300

    qa -> ask db name

--- init-box.sh
+++ init-box.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#sudo gunicorn -c etc/gunicorn_ask.conf.py /home/box/web/ask ask.wsgi:application
+#sudo gunicorn ask.wsgi -c ../../etc/gunicorn_ask.conf.py
 sudo gunicorn hello:app -c etc/gunicorn.conf.py &
 #sudo /etc/init.d/mysql start
